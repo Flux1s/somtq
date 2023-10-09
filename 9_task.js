eec860434c411c462b5ef7a676a56976a6cbfd82
Flux1s <keronjenos@gmail.com>
2023-10-09 10:13:17 +0300

    yeah

--- 9_task.js
+++ 9_task.js
@@ -4,5 +4,5 @@ const loanTermYear = 2;
 let overpaid = (creditRub * interestRatePerc / 100) * loanTermYear;
 let totalPayment = creditRub + overpaid;
 
-console.log(totalPayment)
-console.log(overpaid)
+console.log(totalPayment); 
+console.log(overpaid);
