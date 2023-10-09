fdc6a6841d899bbc9a21a8bbd2fbb01bb41c2ba1
Flux1s <keronjenos@gmail.com>
2023-10-09 10:16:49 +0300

    guessANumber

---
+++ 10_task.js
@@ -0,0 +1,6 @@
+const bill = 3500;
+const tip = 500;
+const friendsCheck = 8;
+let shareOfBill = (bill + tip) / friendsCheck;
+
+console.log("С каждого по " + shareOfBill + " рублей, при общем счёте " + bill + " рублей и чаевыми " + tip +" рублей");
